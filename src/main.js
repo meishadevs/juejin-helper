5c7508c6f10d03e5382cfe68689125dde375caf6
fyt <meishadevs@gmail.com>
2024-04-16 16:56:15 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ const main = async () => {
 
     growth.userName = juejin.user.user_name
   } catch {
-    throw new Error('登录失败, 请尝试更新 Cookies:' + COOKIE)
+    throw new Error('登录失败, 请尝试更新 Cookies')
   }
 
   // 签到
